225437b9d1fe2832e40f192a112f0c6f731973d2
Argentum11 <92793837+Argentum11@users.noreply.github.com>
2025-01-06 20:09:24 +0800

    feat(theme-buttons): navigate to phone call page on button press

--- lib/pages/theme_selection_page.dart
+++ lib/pages/theme_selection_page.dart
@@ -4,62 +4,45 @@ import 'fake_phone_call.dart';
 class ThemeSelectionPage extends StatelessWidget {
   const ThemeSelectionPage({super.key});
 
-  void _onAiConversationPressed() {
-    // 預留
-  }
-
-  void _onTheme1Pressed() {
-    // 預留
-  }
-
-  void _onTheme2Pressed(BuildContext context) {
-    //測試用
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const FakePhoneCallPage()),
-    );
-  }
-
-  void _onTheme3Pressed() {
-    // 預留
-  }
-
-  Widget _buildButton(String text, VoidCallback onPressed) {
-    return Expanded(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            width: 200, // button 寬
-            height: 60, // button 高
-            child: ElevatedButton(
-              onPressed: onPressed,
-              child: Text(
-                text,
-                style: const TextStyle(fontSize: 18),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Theme Selection Page')),
-      body: Column(
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          _buildButton('AI conversation', _onAiConversationPressed),
-          _buildButton('Theme 1', _onTheme1Pressed),
-          _buildButton(
-            //測試用
-            'Theme 2',
-            () => _onTheme2Pressed(context),
-          ),
-          _buildButton('Theme 3', _onTheme3Pressed),
+          ThemeButton(theme: 'AI conversation'),
+          ThemeButton(theme: 'Theme 1'),
+          ThemeButton(theme: 'Theme 2'),
+          ThemeButton(theme: 'Theme 3'),
         ],
+      ),
+    );
+  }
+}
+
+class ThemeButton extends StatelessWidget {
+  final String theme;
+  const ThemeButton({super.key, required this.theme});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const FakePhoneCallPage()),
+          );
+        },
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(60, 0),
+        ),
+        child: Text(
+          theme,
+          style: const TextStyle(fontSize: 18),
+        ),
       ),
     );
   }
